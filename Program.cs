e780b087c158090bdce428a68ed8074eea8d9d15
Thomas-Gilligan-Student <thgi236@student.oaisd.org>
2024-10-10 01:20:49 +0000

    Created simple program to calculate a letter grade from the inputted number grade.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+
+    public static void Main(string[] args) {
+        Console.Clear();
+        Console.WriteLine("Enter your number score:");
+        float gradeNum = float.Parse(Console.ReadLine() ?? "0");
+        getResult(gradeNum);
+
+    }
+
+    public static void getResult(float number) {
+
+        switch (number) {
+            case >= 100:
+                Console.WriteLine("Wow! 100...That's impressive!\nLetter Grade: A+"); break;
+            case >= 90:
+                Console.WriteLine("Very nice!\nLetter Grade: A"); break;
+            case >= 80:
+                Console.WriteLine("Keep it up!\nLetter Grade: B"); break;
+            case >= 70:
+                Console.WriteLine("You got this, keep working!\nLetter Grade: C"); break;
+            case >= 60:
+                Console.WriteLine("Keep trying!\nLetter Grade: D"); break;
+            default:
+                Console.WriteLine("Don't give up!\nLetter Grade: E"); break;
+                
+        }
+    }
+}
